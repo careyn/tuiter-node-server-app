50c41070a1bf234c5b3d3494cb1bcf28498a49e2
careyn <carey.n@northeastern.edu>
2022-11-25 19:41:13 -0500

    a9 dpme

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-import express from 'express';
-import HelloController from "./controllers/hello-controller.js";
-import UserController from "./controllers/users/users-controller.js";
-import TuitsController from "./controllers/tuits/tuits-controller.js";
-import cors from 'cors';
-const app = express();
-app.use(cors());
-app.use(express.json());
-HelloController(app);
-UserController(app);
-TuitsController(app);
-app.listen(process.env.PORT || 4000);
+import mongoose from "mongoose";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+ || 'mongodb://localhost:27017/tuiter';
+mongoose.connect(CONNECTION_STRING);
