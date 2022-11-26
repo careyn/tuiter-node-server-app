c10b06a624e519a6df2facc0b0031b55caca3e5c
careyn <carey.n@northeastern.edu>
2022-11-25 20:14:29 -0500

    fixing imports, app

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 import mongoose from "mongoose";
+import express from 'express';
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./controllers/users/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+import cors from 'cors';
+
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
- || 'mongodb://localhost:27017/tuiter';
+ || 'mongodb://localhost:27017/tuiter/';
 mongoose.connect(CONNECTION_STRING);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+HelloController(app);
+UserController(app);
+TuitsController(app);
+app.listen(process.env.PORT || 4000);
